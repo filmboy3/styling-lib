09bbeebfecab2cd30b4b9b3eee9844d4fdffe36e
Jonathan Schwartz <filmboy3@yahoo.com>
2019-02-04 16:14:26 -0500

    added styled button

--- docs/App.js
+++ docs/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-// import { H1 } from 'styledbymoi'
+import { H1, H2, H3, H4, Button } from 'styledbymoi'
 
 const App = () => 
     <div>
-        {/* <H1>Hello world</H1> */}
+        <H1>Hello World!</H1>
+        <H2>Hello H2!</H2>
+        <H3>Hello H3!</H3>
+        <H4>Hello H4!</H4>
+        <Button>Hello Button!</Button>
+       
     </div>
-
 
 export default App
